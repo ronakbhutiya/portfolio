09e511c0ed3eb310036ac84b813101f778cd0ad3
ronakbhutiya <ronakbhutiya123@gmail.com>
2024-06-01 13:11:26 +0530

    add project and java script

---
+++ js.js
@@ -0,0 +1,5 @@
+const nav_header = document.querySelector(".header-nav");
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+mobile_nav.addEventListener("click", () => {
+  nav_header.classList.toggle("active");
+});
