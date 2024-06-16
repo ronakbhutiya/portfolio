1af81686d1979ddb508594c4879b66bf3ebdeeda
ronakbhutiya <ronakbhutiya123@gmail.com>
2024-06-16 19:50:46 +0530

    add folder

--- js.js
+++ js.js
@@ -4,3 +4,18 @@ mobile_nav.addEventListener("click", () => {
   nav_header.classList.toggle("active");
 });
 //-------------------------STICKY NAVIGATION----------------------------
+// const header = document.querySelector(".header");
+// const observer = new IntersectionObserver(
+//   (entries) => {
+//     const ent = entries[0];
+//     ent.isIntersecting === false
+//       ? nav__header.classList.add("sticky")
+//       : nav__header.classList.remove("sticky");
+//   },
+//   {
+//     root: null,
+//     rootMargin: "",
+//     threshold: "",
+//   }
+// );
+// observer.observe(header);
